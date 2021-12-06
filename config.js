796286056ad219db5c470da298b5bccb79ab9212
Warma <noreply@example.com>
2021-12-06 15:54:39 +0000

    Automatic commit

---
+++ config.js
@@ -0,0 +1,13 @@
+window.GeneralConfig = {
+    // network: "rinkeby",
+
+    onlineChainId: '0x38',
+    chainName: 'Binance Smart Chain Mainnet',
+    nativeCurrency: {
+        name: 'Binance Chain Native Token',
+        symbol: 'BNB',
+        decimals: 18
+    },
+    rpcUrls: ['https://wwww.binance.org/'],
+    blockExplorerUrls: ['https://bscscan.com']
+};
