01d6caf33f631c377aa7548651411e3570916e37
Warma <noreply@example.com>
2022-01-05 17:31:02 +0000

    Automatic commit

--- config.js
+++ config.js
@@ -1,27 +1,31 @@
 window.GeneralConfig = {
     network: "rinkeby",
 
+    baseToken: '0x962bF03f58CB853C90BfEA71EeeDd2b6005D4925',
+    nftMaster: '0xb02EAefca8Dd476Ea7aC09960beBE6Fd82bE704A',
+    ironToken: '0x5fa543a064723959E7E65e68d64F89115963e912',
 
-    baseToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-
-    ironToken: "0x130F915b1ABC77506874Fcd63133cEB50a4bB6eD",
-    nftMaster: "0xD10d9b87D117416D1ca737081771976C0B87D356",
     mockX21: "0x9675deffCE2723375687dDcBA059d800F9750F43",
     mockX24: "0xB1c8d8A404a42298ad714Bb9dc6B60697f2c5B82",
     mockX27: "0x6775473Cf80059Dd6cc028fc6ba3eCd3D0337191",
     mockX29: "0x328873331566d1a4eCC5f943854dD7d7F2e664bD",
-    market: "0x7f92AA4b7da43Ea7f1Ec01276a671C147e62F074",
 
-    price:0.1,
+    // 'baseToken': "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    // 'ironToken': "0x130F915b1ABC77506874Fcd63133cEB50a4bB6eD",
+    // 'nftMaster': "0xD10d9b87D117416D1ca737081771976C0B87D356",
+    // "mockX21": "0x9675deffCE2723375687dDcBA059d800F9750F43",
+    // "mockX24": "0xB1c8d8A404a42298ad714Bb9dc6B60697f2c5B82",
+    // "mockX27": "0x6775473Cf80059Dd6cc028fc6ba3eCd3D0337191",
+    // "mockX29": "0x328873331566d1a4eCC5f943854dD7d7F2e664bD",
 
 
-    onlineChainId: '0x61',
-    chainName: 'Smart Chain - Testnet',
-    nativeCurrency: {
-        name: 'Binance Chain Native Token',
-        symbol: 'BNB',
-        decimals: 18
-    },
-    rpcUrls: ['https://wwww.binance.org/'],
-    blockExplorerUrls: ['https://bscscan.com']
+    // onlineChainId: '0x38',
+    // chainName: 'Binance Smart Chain Mainnet',
+    // nativeCurrency: {
+    //     name: 'Binance Chain Native Token',
+    //     symbol: 'BNB',
+    //     decimals: 18
+    // },
+    // rpcUrls: ['https://wwww.binance.org/'],
+    // blockExplorerUrls: ['https://bscscan.com']
 };
