81e73d32eb2c33b9a6643191486e030f89345846
Warma <noreply@example.com>
2022-01-07 06:32:29 +0000

    Automatic commit

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 window.GeneralConfig = {
-    network: "rinkeby",
+    network: "bsc",
 
+    // BSC Testnet config
+    //<<<
+    // baseToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    // ironToken: "0xCAa09e779408Db48C6ad303e8ac93bfD0d89F753",
+    // iisToken: "0x02aaf45D00955521F2Bfc74fD9f4Bc756773E65E",
+    // market: "0xFaE370868fe79211dF4d2B020B35AA3818aee41e",
 
-    baseToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    // nftMaster: "0x229fEF39399A1d6bb112Eb69A57972DBB796F231",
+    // mockX21: "0x5e53907896A90098560d24Eb52686dfa9D5E6544",
+    // mockX24: "0x0F1E6f55ba095cf8A87C214a300491A3a852CaAd",
+    // mockX27: "0x19Df2894AfF7427F56dA6Df926C2D30c84FB6B04",
+    // mockX29: "0xF811c5346717a89Ab3BFCf0d03b23463126b72F9",
 
-    ironToken: "0xCAa09e779408Db48C6ad303e8ac93bfD0d89F753",
-    iisToken: "0x02aaf45D00955521F2Bfc74fD9f4Bc756773E65E",
-    market: "0xFaE370868fe79211dF4d2B020B35AA3818aee41e",
-
-    nftMaster: "0xd9595710d6e6F786C89E0Ef4436A73e9d3bC41B9",
-    mockX21: "0xe696B51843CdE9110a3164b1bdb099E09FDc9d92",
-    mockX24: "0x031d9d79C62769BF0B6fEdA903090B476811073b",
-    mockX27: "0x319Ea1B4F354f7E8030abF67eD8DA998FdaAeF51",
-    mockX29: "0xa4152c13003b3f7f28D83c0831d4E8052FcA249D",
-
-    price: 0.1,
+    // price: 0.5,
 
     // onlineChainId: '0x61',
     // chainName: 'Binance Smart Chain - Testnet',
@@ -25,6 +25,21 @@ window.GeneralConfig = {
     // },
     // rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
     // blockExplorerUrls: ['https://testnet.bscscan.com']
+    //>>>
+
+    // BSC Mainnet config
+    baseToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    ironToken: "0x75C1F920F1D173f89eC436ab8DEb7967dD4Aea0B",
+    iisToken: "0xF0e00076b0403395736180C65d5f47B2Ff4F2a5B",
+    market: "0x6221E0a0DE20D590D77f6D646Fc960E98A8DD89B",
+
+    nftMaster: "0xa7e125989B1011B05442857C51cd15C4FdA92c26",
+    mockX21: "0x47e6001aAe817aC288Dc32D7C64bbeA6A941338e",
+    mockX24: "0xE1855dce191a58d815feCf242047782C22c90617",
+    mockX27: "0xaC06d99E6a5e03C9DE5d7Bb51303F7245FC908e1",
+    mockX29: "0x55363b598Dbb32BbAa42195f4f0df01e6c0B1c0e",
+
+    price: 0.5,
 
     onlineChainId: '0x38',
     chainName: 'Binance Smart Chain Mainnet',
